141cb5d77265a034bd2834675ab5e3edc09ef821
Yumingyuan <timdavid@vip.qq.com>
2019-04-24 15:41:46 +0800

    add echo with change output string's color

--- change_echo_background.sh
+++ change_echo_background.sh
@@ -3,3 +3,4 @@
 echo -e "\e[1;31mThis is\e[1;32m green?\e[0m"
 echo -e "\e[1;31mhello\e[1;33m yellow?\e[0mno color?"
 echo -e "\e[1;36mhello\e[1;33m yellow?\e[0mrecover color to no color"
+echo -e "\e[1;36mhello\e[1;37m white?\e[0mrecover color to no color"
