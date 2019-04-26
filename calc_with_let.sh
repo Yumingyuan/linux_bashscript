ca6bab921141a8fd5098a87b1b596f319a415a7b
Yumingyuan <timdavid@vip.qq.com>
2019-04-26 14:52:18 +0800

    use let to calc num1+num2

--- calc_with_let.sh
+++ calc_with_let.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+add()
+{
+	if [ "$1" -gt 0 -a "$2" -gt 0 ] 2>/dev/null;then
+		let result=$1+$2
+		echo "result:$result"
+	else
+		echo "not num" 
+	fi
+}
 opt1=4;
 opt2=5;
 let result=opt1+opt2
@@ -7,3 +16,5 @@ let opt1++
 echo $opt1
 let opt1+=5
 echo $opt1
+add hi te
+add 134234 3242134
