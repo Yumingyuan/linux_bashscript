a4c0eb0ebda12a21c92b8dfded94103004ef191a
Yumingyuan <timdavid@vip.qq.com>
2019-04-26 14:35:40 +0800

    change $PATH var by bash

--- add_env_var.sh
+++ add_env_var.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+prepend()
+{
+	[ -d "$2" ] && eval $1="\$$1:$2" && export $1;
+}
+prepend_test()
+{
+	[ -d "$2" ] && eval $1=\"\$$1':'$2\" && export $1;
+}
 echo $PATH
 echo "Add some path at PATH"
 export PATH=$PATH:/home/bash_script
+echo $PATH
+prepend PATH /home/
+echo "add by function prepend"
+echo $PATH
+prepend_test PATH /etc
+echo "add by function prepend_test"
 echo $PATH
