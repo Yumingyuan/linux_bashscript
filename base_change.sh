118ad23917d076757c00507f1c997574ff912b63
Yumingyuan <timdavid@vip.qq.com>
2019-04-28 12:18:02 +0800

    bash进制转换

---
+++ base_change.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#作用：进制转换
+no=100
+result=`echo "obase=2;$no"|bc`
+echo -e "\e[1;31mresult:\e[0m"$result
+result1=`echo "obase=10;ibase=2;$result"|bc`
+echo -n "with out ibase's result:"
+echo "obase=10;$result"|bc
+echo -e "\e[1;31mresult:\e[0m"$result1
+
