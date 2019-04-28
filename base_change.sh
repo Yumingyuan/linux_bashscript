6dc68bf4c4b99b8db17e6c8d42ab33d9f0116c65
Yumingyuan <timdavid@vip.qq.com>
2019-04-28 12:30:44 +0800

    bash进制转换

--- base_change.sh
+++ base_change.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#作用：进制转换
+#作用：进制转换，开根号和平方计算
 no=1000
 result=`echo "obase=2;$no"|bc`
 echo -e "from $no to Binary \e[1;31mresult:\e[0m"$result
@@ -7,4 +7,6 @@ result1=`echo "obase=10;ibase=2;$result"|bc`
 echo -n "with out ibase's result:"
 echo "obase=10;$result"|bc
 echo -e "\e[1;31mresult:\e[0m"$result1
-echo "sqrt(100)|bc"#平方根计算
+echo "sqrt(100)"|bc#平方根计算
+echo "10^10"|bc#平方运算
+
