4cea2ca8c246d363429f77c5207061838cc23d68
Yumingyuan <timdavid@vip.qq.com>
2019-04-29 12:57:23 +0800

    自定义文件描述符

--- mannual_file_discriptor.sh
+++ mannual_file_discriptor.sh
@@ -7,8 +7,10 @@ cat <&3
 #截断写入模式
 touch test_out_put.txt
 exec 4>test_out_put.txt
+#对4号描述符操作
 echo -e "\e[1;31mnewline\e[0m">&4
 touch test_out_put.txt
+#对5号描述符操作
 exec 5>>test_out_put.txt
 echo -e "\e[1;37msecond line\e[0m">&5
 cat test_out_put.txt
