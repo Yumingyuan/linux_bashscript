ca910e4f3ca5e41902c77e34c478f5437ece64f1
Yumingyuan <timdavid@vip.qq.com>
2019-05-01 11:15:55 +0800

    数组测试

--- array_test.sh
+++ array_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#创建数组
 array_var=(test1 test2 test3 test4)
+#输出数组中的0号下标元素
 echo -e "\e[1;31meecho first element by 0 :\e[0m"${array_var[0]}
 index=3
 echo -e "\e[1;31meecho third element by index :\e[0m"${array_var[$index]}
+#输出数组所有元素
 echo -e "\e[1;31mecho all elements use by * :\e[0m" ${array_var[*]}
