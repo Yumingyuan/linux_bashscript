b833ba6dcdee7f76774db1534085f08b3feff34b
Yumingyuan <timdavid@vip.qq.com>
2019-05-01 11:23:26 +0800

    数组测试

--- array_test.sh
+++ array_test.sh
@@ -5,5 +5,7 @@ array_var=(test1 test2 test3 test4)
 echo -e "\e[1;31meecho first element by 0 :\e[0m"${array_var[0]}
 index=3
 echo -e "\e[1;31meecho third element by index :\e[0m"${array_var[$index]}
-#输出数组所有元素
+#输出数组所有元素通过*
 echo -e "\e[1;31mecho all elements use by * :\e[0m" ${array_var[*]}
+#输出数组所有元素通过@
+echo -e "\e[1;31mecho all elements use by * :\e[0m" ${array_var[@]}
