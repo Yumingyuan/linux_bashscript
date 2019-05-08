ce7d09bff4a3f9b713b932007e9d9fc1d1b4c243
Yumingyuan <timdavid@vip.qq.com>
2019-05-08 20:19:47 +0800

    获取网卡IP

--- get_interface_information.sh
+++ get_interface_information.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 function getIP()
 {
-	ifconfig $1|grep 'inet '|cut -d ' ' -f10;
+	echo -n " IP:"
+	ifconfig $1|grep 'inet '|cut -d ' ' -f10
+}
+function getIP_with_sed()
+{
+	ip_information=`ifconfig $1|grep '^ *inet\ '|sed 's/^ *.*inet //g'|cut -d ' ' -f 1`
+	netmask_information=`ifconfig $1|grep '^ *inet\ '|sed 's/^.*netmask //g'`
+	echo -e "\e[1;31m IP:\e[0m$ip_information" $netmask_information
 }
 getIP eth0
 getIP ens32
-getIP lo
+getIP_with_sed lo
