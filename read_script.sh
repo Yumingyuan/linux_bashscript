8e6e686c8038b875628ccf7841ff13977dc16063
Yumingyuan <timdavid@vip.qq.com>
2019-05-17 15:56:45 +0800

    read输入两字符和定界符:\-n和\-d

--- read_script.sh
+++ read_script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #read从输入读取n个字符并存入某个变量
+#read从输入读变量当遇到特定定界符号结束
 echo -ne "\e[1;31minput 2 chars\e[0m:"
 read -n 2 var
 echo ""
 echo -e "\e[1;31mvar you input\e[0m:"$var
+echo -ne "\e[1;33minput when get '&' end input\e[0m:"
+read -d "&" var
+echo ""
+echo -e "\e[1;33mvar you input\e[0m:"$var
