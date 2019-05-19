d6907919f7a91505605404976b2bf2f65a954a68
Yumingyuan <timdavid@vip.qq.com>
2019-05-19 10:42:47 +0800

    passwd字段分隔符测试脚本

--- IFS_CSV_data_echo.sh
+++ IFS_CSV_data_echo.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 #测试$IFS变量
-data="name,gender,roolno,location"
-OLDIFS=$IFS
-IFS=,
+data=`cat /etc/passwd`
+echo "$data"
+#OLDIFS=$IFS
+#IFS="$"
 for item in $data;
 do
-	echo "Item:"$item
+	echo "current item:"$item
+	OLDIFS=$IFS
+	IFS=:
+	for str in $item;
+	do
+		echo "small item:"$str
+	done
+	IFS=$OLDIFS
+	echo ""
 done
 
