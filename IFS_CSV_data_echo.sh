e8c68c401d6eaca1e357c18478aba4124291e9bc
Yumingyuan <timdavid@vip.qq.com>
2019-05-22 08:44:09 +0800

    内部字段分隔符

--- IFS_CSV_data_echo.sh
+++ IFS_CSV_data_echo.sh
@@ -16,4 +16,15 @@ do
 	IFS=$OLDIFS
 	echo ""
 done
-
+line="root:x:0:0:root:/root:/bin/bash"
+oldIFS=$IFS;
+IFS=":"
+count=0
+for item in $line;
+do
+	[ $count -eq 0 ]&&user=$item
+	[ $count -eq 6 ]&&shell=$item
+	let count++
+done
+IFS=$oldIFS
+echo -e "\e[1;31muser\e[0m:$user used shell:$shell"
