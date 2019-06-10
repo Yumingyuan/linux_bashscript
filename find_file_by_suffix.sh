5eace503e3eb1a2b2a5de41ea46a59fdc5b65789
Yumingyuan <timdavid@vip.qq.com>
2019-06-10 13:57:34 +0800

    根据后缀名查找文件

--- find_file_by_suffix.sh
+++ find_file_by_suffix.sh
@@ -3,11 +3,7 @@ echo -en "\e[1;31minput the search dir\e[0m:"
 read dir
 echo -en "\e[1;32minput the suffix want to_search\e[0m:"
 read suffix
-if [ -z $suffix ];
-then
-	exit 1
-fi
-if [ -d $dir ];
+if [ -d $dir -a -n $suffix ];
 then
 	find $dir -name "*.$suffix"
 else
