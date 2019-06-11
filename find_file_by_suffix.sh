8729de27353be33b55f7dd02791542535ffad3b7
Yumingyuan <timdavid@vip.qq.com>
2019-06-11 18:40:00 +0800

    根据后缀名查找文件

--- find_file_by_suffix.sh
+++ find_file_by_suffix.sh
@@ -5,7 +5,7 @@ echo -en "\e[1;32minput the suffix want to_search\e[0m:"
 read suffix
 if [ -d $dir -a -n $suffix ];#如果文件夹存在，且后缀不为空字符串
 then
-	find $dir -name "*.$suffix"
+	find $dir -iname "*.$suffix"
 else
 	exit 1
 fi
