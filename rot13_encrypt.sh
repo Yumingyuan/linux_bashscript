50237e7347d6d8dfbf1c4951ed0c1b4af3520a41
Yumingyuan <timdavid@vip.qq.com>
2019-07-13 16:08:15 +0800

    rot13加密

--- rot13_encrypt.sh
+++ rot13_encrypt.sh
@@ -3,7 +3,7 @@
 echo -n "input string u want to encrypt:"
 read string
 encrypt_string=`echo $string|tr 'a-zA-Z' 'n-za-mN-ZA-M'`
-echo -n "after encrypt "$string " to:"$encrypt_string
+echo -n "after encrypt "$string" to:"$encrypt_string
 decrypt_string=`echo $encrypt_string | tr 'n-za-mN-ZA-M' 'a-zA-Z'`
 echo -e "\nafter decrypt "$encrypt_string" to:"$decrypt_string
 
