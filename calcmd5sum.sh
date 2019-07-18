d49c81c13805df51415129c59f20f527cbd5051f
Yumingyuan <timdavid@vip.qq.com>
2019-07-18 20:17:37 +0800

    计算md5校验和的脚本

--- calcmd5sum.sh
+++ calcmd5sum.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #计算md5校验和
-#rm -f current_dir.md5#每一次运行前删除，要不然是追加
+rm -f current_dir.md5#每一次运行前删除，要不然是追加
 find ./ -type f -name "*.sh" -print0 |xargs -0 md5sum >> current_dir.md5
 md5sum -c current_dir.md5
