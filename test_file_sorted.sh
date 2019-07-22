4f6f8946716498e011a6a4d6edfde4cd6cc2e74c
Yumingyuan <timdavid@vip.qq.com>
2019-07-22 16:58:35 +0800

    检测文件是否已经排过序

--- test_file_sorted.sh
+++ test_file_sorted.sh
@@ -2,3 +2,14 @@
 #检验文件是否已经被排序了
 echo -n "input the file name:"
 read  file_name
+if [ -e $file_name  ];then
+	sort -C $file_name
+	if [ $? -eq 0 ];then
+		echo "Sorted"
+	else
+		echo "Unsorted"
+	fi
+else
+	echo "not exist"
+	exit 2
+fi
