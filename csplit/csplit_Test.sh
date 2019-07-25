edb7856eba2ec7d78a9e3132606a76b81bc7a6c1
Yumingyuan <timdavid@vip.qq.com>
2019-07-25 14:23:04 +0800

    csplit根据文件内容上下文进行分割操作

--- csplit/csplit_Test.sh
+++ csplit/csplit_Test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #csplit分割演示
-csplit server.log /SERVER/ -s -n 2 {1} -f server -b "%02d.log"i
+csplit server.log /SERVER/ -s -n 2 {1} -f server -b "%02d.log"
 echo "split ok!"
 find . -regex ".*server[0-9]+.log"
 echo "now remove splited files"
