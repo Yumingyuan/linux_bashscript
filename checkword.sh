e8442b331ff5b685f1d86abaab662b2c0b77ec5c
Yumingyuan <timdavid@vip.qq.com>
2019-07-30 17:05:17 +0800

    检查单词是否存在于词典文件中

--- checkword.sh
+++ checkword.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #作用：检查给丁单词是否存在于linux /usr/share/dict字典文件中
 word=$1
-grep "^$word$" /usr/share/dict/linux.words
+grep "^$word$" /usr/share/dict/linux.words -q
+if [ $? -eq 0 ];then
+	echo $word is in dictionary
+else
+	echo $word is not in dictionary
+fi
