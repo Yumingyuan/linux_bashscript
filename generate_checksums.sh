b8dc06d1f1e131ff2e8af4751aa5b753936c985e
Yumingyuan <timdavid@vip.qq.com>
2019-08-05 12:42:08 +0800

    多进程执行

---
+++ generate_checksums.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#并行进程计算md5校验和
+PIDARRAY=()
+for file in add_env_var.sh add_account_by_forstatement.sh base64_encode.sh
+do
+	md5sum $file &
+	PIDARRAY+=("$!")
+	echo $!
+done
+#echo ${PIDARRAY[@]}#获取数组中所有数据
+wait ${PIDARRAY[@]}#等待数组中所有进程结束
