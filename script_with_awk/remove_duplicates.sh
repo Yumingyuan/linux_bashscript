00be71339f669c881307b2c47cee353de5834984
Yumingyuan <timdavid@vip.qq.com>
2019-08-07 15:50:48 +0800

    remove duplicate file

---
+++ script_with_awk/remove_duplicates.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#用途：查找并删除重复文件，每个文件仅仅保留一份
+ls -lS --time-style=long-iso |awk 'BEGIN {
+	getline; getline;
+	name1=$8;size=$5
+}
+{	
+	name2=$8;
+	if (size==$5)
+	{
+		"md5sum "name1; csum1=$1;
+		"md5sum "name2; csum2=$1;
+		if ( csum1==csum2 )
+		{
+			print name1;print name2
+		}
+	};
+
+
+size=$5;
+name1=name2;
+}'|sort -u > duplicate_files
+cat duplicate_files|xargs md5sum|sort|uniq -w 32|awk '{print $2}'|sort -u > unique_files
+echo Removing
+comm duplicate_files unique_files -3|tee /dev/strerr
+echo Removed duplicates file successful
