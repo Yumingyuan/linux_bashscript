512c273884781a08383daa305fead2a251d6d222
Yumingyuan <timdavid@vip.qq.com>
2019-09-16 16:46:56 +0800

    grep -q的实验，测试文件是否包含特定文本内容，接受两个命令行参数，如果存在则提示exist，不存在则提示不存在

---
+++ silent_grep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#使grep以quiet模式运行，测试文件是否有相应文本内容
+if [ $# -ne 2 ];then
+	echo "Usage: $0 match_text filename"
+	exit 1
+fi
+match_text=$1
+filename=$2
+grep -q "$match_text" $filename
+
+if [ $? -eq 0 ];then
+	echo "The text exists in the file"
+else
+	echo "Text does not exist in the file"
+fi
